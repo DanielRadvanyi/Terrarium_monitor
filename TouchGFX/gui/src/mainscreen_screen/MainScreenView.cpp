748b15b1dd482d0db0505db3e3dd656b8861457b
Daniel Radvanyi <70892020+DanielRadvanyi@users.noreply.github.com>
2024-02-07 16:35:59 +0200

    added hardcoded indicator logic

--- TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
+++ TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
@@ -1,4 +1,5 @@
 #include <gui/mainscreen_screen/MainScreenView.hpp>
+#include <touchgfx/Color.hpp>
 
 MainScreenView::MainScreenView()
 {
@@ -18,6 +19,15 @@ void MainScreenView::tearDownScreen()
 void MainScreenView::updateTemp(float temp)
 {
 	Unicode::snprintf(TempValueBuffer, TEMPVALUE_SIZE, "%.1f", temp);
+	if(temp < 23.0)
+	{
+		TempIndicatorPainter.setColor(touchgfx::Color::getColorFromRGB(225, 0, 0)); // Red
+	}
+	else
+	{
+		TempIndicatorPainter.setColor(touchgfx::Color::getColorFromRGB(40, 130, 40)); // Dark Green
+	}
+
 	TempValue.resizeToCurrentText();
 	TempValue.invalidate(); // redraw
 }
@@ -25,6 +35,14 @@ void MainScreenView::updateTemp(float temp)
 void MainScreenView::updateHum(unsigned int hum)
 {
 	Unicode::snprintf(HumValueBuffer, HUMVALUE_SIZE, "%d", hum);
+	if(hum < 35 || hum > 85)
+	{
+		HumIndicatorPainter.setColor(touchgfx::Color::getColorFromRGB(225, 0, 0)); // Red
+	}
+	else
+	{
+		HumIndicatorPainter.setColor(touchgfx::Color::getColorFromRGB(40, 130, 40)); // Dark Green
+	}
 	HumValue.resizeToCurrentText();
 	HumValue.invalidate(); // redraw
 }
